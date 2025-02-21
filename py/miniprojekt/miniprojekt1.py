0fb33e4417340f82565bf8945a049ddeb654255c
TopsSDE <77180@edu.sde.dk>
2025-02-21 10:47:53 +0100

    Update miniprojekt1.py

--- py/miniprojekt/miniprojekt1.py
+++ py/miniprojekt/miniprojekt1.py
@@ -1,17 +1,33 @@
-tal = 20
-print("Gæt et tal mellem 1 og 100 ;)")
-guess = int(input())
+import random
 
-if guess == tal:
-    print("Rigtigt!")
-elif guess < 1 or guess > 100:
-    print("Uden for den angivne interval på 1-100")
-else:
-    difference = abs(guess - tal)
-    if difference > 50:
-        print("Meget langt forbi")
-    elif 19 < difference <= 49:
-        print("Du er ikke helt ved siden af")
-    else:
-        print("Tampen brænder!")
+while True:
+    tal = random.randint(1, 100)
+    print(tal)
+    forsøg = 0
+    print("Gæt et tal mellem 1 og 100 ;)")
 
+    while True:
+        guess = int(input())
+        forsøg += 1
+        print("Det er dit forsøg nr, ", forsøg)
+        if guess == tal:
+            print(f"Rigtigt! Du gættede det på {forsøg} forsøg.")
+            break
+        elif guess < 1 or guess > 100:
+            print("Uden for den angivne interval på 1-100")
+        else:
+            difference = abs(guess - tal)
+            if difference > 50:
+                print("Meget langt forbi")
+            elif 19 < difference <= 49:
+                print("Du er ikke helt ved siden af")
+            else:
+                print("Tampen brænder!")
+        if forsøg == 10:
+            print("Du har ikke flere forsøg")
+            break
+
+    igen = input("Vil du prøve igen? (y/n): ").lower()
+    if igen != "y":
+        print("Farvel!")
+        break
